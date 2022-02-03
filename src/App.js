50e241a80de45c3ce58a998678e7028ab9811db4
Smoke52ru <Smoke52ru@ya.ru>
2022-02-03 19:28:30 +0300

    Реализован поиск, сортировка и хук useMemo()

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css';
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'},
+        {id: 1, title: 'Aaa', body: 'Sss'},
+        {id: 2, title: 'Mmm', body: 'Www'},
+        {id: 3, title: 'Hhh', body: 'Bbb'},
     ])
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const sortedPosts = useMemo(() => {
+            console.log('sorted...')
+            if (selectedSort) {
+                return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+            }
+            return posts
+        }, [selectedSort, posts]
+    )
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+    }, [searchQuery, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -21,6 +36,10 @@ function App() {
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
     }
 
     return (
@@ -31,9 +50,27 @@ function App() {
             <ClassCounter/>
 
             <PostForm create={createPost}/>
-            {posts.length
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MyInput
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="Поиск..."
+                    type="text"
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка по..."
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'},
+                    ]}
+                />
+            </div>
+            {sortedAndSearchedPosts.length
                 ?
-                <PostList remove={removePost} posts={posts} title="Список постов"/>
+                <PostList remove={removePost} posts={sortedPosts} title="Список постов"/>
                 :
                 <h1 style={{textAlign: 'center'}}>
                     Посты не найдены!
