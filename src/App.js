9a7c67acece1ce98d5ee1dcd84baef2fd4402b95
Smoke52ru <Smoke52ru@ya.ru>
2022-02-03 18:35:10 +0300

    1-st hour of video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+    ])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+
+    return (
+        <div className="App">
+            {/*Функциональная компонента*/}
+            <Counter/>
+            {/*Классовая компонента*/}
+            <ClassCounter/>
+
+            <PostForm create={createPost}/>
+            <PostList posts={posts} title="Список постов"/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
+
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({
+        title: '',
+        body: '',
+    })
+
+    const addNewPost = (event) => {
+        event.preventDefault()
+        const newPost = {
+            ...post,
+            id: Date.now()
+        }
+        create(newPost)
+        setPost({title: '',body: '',})
+    }
+
+    return (
+        <form>
+            {/*Управляемый компонент*/}
+            <MyInput
+                value={post.title}
+                onChange={(e) => setPost({...post, title: e.target.value})}
+                type="text"
+                placeholder="Название поста"
+            />
+            <MyInput
+                value={post.body}
+                onChange={(e) => setPost({...post, body: e.target.value})}
+                type="text"
+                placeholder="Описание поста"
+            />
+            <MyButton onClick={addNewPost}>Создать пост</MyButton>
+        </form>
+    );
+};
+
+export default PostForm;
