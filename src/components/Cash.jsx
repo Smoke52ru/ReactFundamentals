154ffed9a72f764b64091981133decbe5b7d0968
Smoke52ru <Smoke52ru@ya.ru>
2022-02-16 15:48:37 +0300

    install redux-saga, added action creators, reduxDevTools, styles refactored

--- src/components/Cash.jsx
+++ src/components/Cash.jsx
@@ -1,26 +1,57 @@
 import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import MyButton from "./UI/button/MyButton";
+import {addCustomerAction, removeCustomerAction} from "../store/customerReducer";
+import classes from './Cash.module.css'
 
 const Cash = () => {
     const dispatch = useDispatch()
     const cash = useSelector(state => state.cash.cash)
-    console.log(cash)
+    const customers = useSelector(state => state.customers.customers)
 
     function addCash(cash) {
-        dispatch({type: "ADD_CASH", payload:cash})
+        dispatch({type: "ADD_CASH", payload: cash})
     }
 
     function getCash(cash) {
-        dispatch({type: "GET_CASH", payload:cash})
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+
+    function addCustomer(name) {
+        const customer = {
+            name,
+            id: Date.now(),
+        }
+        dispatch(addCustomerAction(customer))
+    }
+
+    function removeCustomer(customer) {
+        dispatch(removeCustomerAction(customer.id))
     }
 
     return (
-        <div className='app'>
-            <div style={{fontSize:'3rem'}}>{cash}</div>
-            <div style={{display: 'flex'}}>
-                <MyButton onClick={()=> addCash(Number(prompt()))}>Пополнить счет</MyButton>
-                <MyButton onClick={()=> getCash(Number(prompt()))}>Снять со счета</MyButton>
+        <div className={classes.cashApp}>
+            <div className={classes.cashCounter}>{cash}</div>
+            <div className={classes.cashButtons}>
+                <MyButton onClick={() => addCash(Number(prompt()))}>Пополнить счет</MyButton>
+                <MyButton onClick={() => getCash(Number(prompt()))}>Снять со счета</MyButton>
+                <MyButton onClick={() => addCustomer(prompt())}>Добавить клиента</MyButton>
+            </div>
+            <div className={classes.customersList}>
+                {customers.length > 0 ?
+                    <div>
+                        {customers.map(customer =>
+                            <div onClick={()=>removeCustomer(customer)}
+                                className={classes.customersItem}>
+                                {customer.name}
+                            </div>
+                        )}
+                    </div>
+                    :
+                    <div>
+                        Клиенты отсутствуют!
+                    </div>
+                }
             </div>
         </div>
     );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import {combineReducers, createStore} from "redux";
 import {cashReducer} from "./cashReducer";
 import {customerReducer} from "./customerReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
     cash: cashReducer,
     customers: customerReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools())
