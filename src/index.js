4ebfb1c96fb14a0656b4530ab527a471a2f3fbf4
Smoke52ru <Smoke52ru@ya.ru>
2022-02-16 17:32:47 +0300

    store file renamed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-import {store} from "./store";
+import {store} from "./store/store";
 
 
 ReactDOM.render(
