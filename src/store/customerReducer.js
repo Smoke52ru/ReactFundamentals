52a7e685c3236d8ad091a0984e6b217567551ae8
Smoke52ru <Smoke52ru@ya.ru>
2022-02-17 13:37:25 +0300

    minor changes

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -15,7 +15,6 @@ export const customerReducer = (state = defaultState, action) => {
         case REMOVE_CUSTOMER:
             return {...state, customers: state.customers.filter(customer=> customer.id!== action.payload)}
 
-
         default:
             return state
     }
