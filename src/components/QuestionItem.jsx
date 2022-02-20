8fb7ab42b4290e4231a7f6153bec40d911887409
Smoke52ru <Smoke52ru@ya.ru>
2022-02-20 16:48:37 +0300

    names and keys fix

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -12,13 +12,14 @@ const QuestionItem = ({number, text, picture, answers, isMultiple, ...props}) =>
                  src={picture} alt=""/>
             <div className={classes.questionCard__answers}>
                 {answers.map((answer, index) =>
-                    <div key={props.key + '_' + index}>
+                    <div key={answer.id}>
                         <input type={isMultiple ? "checkbox" : "radio"}
-                               name={'answer_' + props.key + '_' + number}
-                               id={'answer_' + props.key + '_' + number + '_' + index}
+                               name={'answers_' + number}
+                               id={'answer_' + number + '_' + index}
                         />
-                        <label
-                            htmlFor={'answer_' + props.key + '_' + number + '_' + index}>{index + 1}. {answer}</label>
+                        <label htmlFor={'answer_' + number + '_' + index}>
+                            {index + 1}. {answer}
+                        </label>
                     </div>
                 )}
             </div>
